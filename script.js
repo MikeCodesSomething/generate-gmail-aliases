ecc4788f51dc6b430b0b2e091a420879e9f8e1e0
MikeCodesSomething <129166471+MikeCodesSomething@users.noreply.github.com>
2023-06-26 17:25:26 +0100

    Add dropdown to choose a number of Aliases to generate Add functionality to make @gmail.com optional input

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.getElementById('generate-button').onclick = function() {
-    email = document.getElementById('email').value
-    for (let i = 1; i <= 5; i++)  {
+    let aliases = document.getElementById('aliases').value
+    let email = document.getElementById('email').value
+    resetAliases()
+    for (let i = 1; i <= aliases; i++)  {
         let resultId = `result${i}`
         let alias = generateAlias(email)
         document.getElementById(resultId).innerHTML = alias
@@ -9,8 +11,15 @@ document.getElementById('generate-button').onclick = function() {
 
 function generateAlias(email) {
     //for now just sticking 1 period after the first character
+    if (email.slice(-10) === "@gmail.com") {
+        email = email.slice(0,-10)
+    }
+    if (email.slice(-15) === "@googlemail.com") {
+        email = email.slice(0,-15)
+    }
+
     let indexesForPeriods = [];
-    for (let i = 1; i <= email.length && email[i] != '@'; i++) {
+    for (let i = 1; i < email.length; i++) {
         if (email[i] != '.' && Math.random() < 0.5) {
             indexesForPeriods.unshift(i)
             // little hack here, using unshift rather than push, because we
@@ -27,10 +36,23 @@ function generateAlias(email) {
     for (i in indexesForPeriods) {
         alias = addPeriodAtIndex(alias,indexesForPeriods[i])
     }
+    if (Math.random() < 0.5) {
+        let domain = "@googlemail.com"
+        alias += domain
+    }
+    else {
+        let domain = "@gmail.com"
+        alias += domain
+    }
     return alias
 }
 
 let addPeriodAtIndex = (string,index) =>
 string.slice(0,index) +'.'+ string.slice(index,string.length)
 
-console.log(addPeriodAtIndex('test',0))
+function resetAliases() {
+    //reset any aliases we set before
+    for (let i = 1; i <= 50; i++) {
+        let resultId = `result${i}`
+        document.getElementById(resultId).innerHTML = ""}
+}
